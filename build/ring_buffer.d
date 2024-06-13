8abba26386f285a17c729a120e2d4f5dfa291839
kong lei <1063299919@qq.com>
2024-06-13 22:08:31 +0800

    add ignorefile

--- build/ring_buffer.d
+++
@@ -1,5 +0,0 @@
-build/ring_buffer.o: App/SmartDevice/ModuleDriver/ring_buffer.c \
- App/SmartDevice/ModuleDriver/ring_Buffer.h \
- App/SmartDevice/SubSystems/system_type.h
-App/SmartDevice/ModuleDriver/ring_Buffer.h:
-App/SmartDevice/SubSystems/system_type.h:
